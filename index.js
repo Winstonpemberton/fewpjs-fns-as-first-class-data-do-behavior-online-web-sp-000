e47276ea97d6650a0c9fc1c35802e4b896059711
Winston Pemberton <winstonpemberton@gmail.com>
2020-11-30 22:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let givenTime = time.split(':');
+  let givenHour = parseInt(givenTime[0]);
+  if (givenHour < 12){
+    return "Good Morning";
+    
+  } else if (nowHour > 17){
+    return "Good Evening";
+    
+  }
+  return "Good Afternoon";
+}
 /* Write your implementation of displayMessage() */
